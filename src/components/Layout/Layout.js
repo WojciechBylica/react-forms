121669ed5325a292185d57e4dc98b3a4bf360787
Wojciech Bylica <wojtekbyl@gmail.com>
2022-04-14 01:32:01 +0200

    added logo to nav; header text upd

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,12 +4,14 @@ import { NavLink } from 'react-router-dom'
 const Layout = ({ children }) => (
   <>
     <nav className="nav">
+      <img src="/logo192.png" width={48} height={48} alt="logo" />
       <NavLink className="nav__link" to="/">
         Formularz
       </NavLink>
     </nav>
     <header>
-      <h1>Formularz z walidacją</h1>
+      <h1>Formularz</h1>
+      <p>napisany bez użycia dodatkowych bibliotek</p>
     </header>
     <main>{children}</main>
     <footer className="footer">stopka</footer>
